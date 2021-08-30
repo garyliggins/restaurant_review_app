a219caa584ae87c65e879ef22f8a6c450240af71
gary <gary@ligginsdevelopment.com>
2021-08-29 17:42:16 -0700

    backend finished

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,16 +1,18 @@
-import express from 'express';
-import RestaurantsCtrl from './restaurants.controller.js'
-import ReviewsCtrl from './reviews.controller.js'
+  
+import express from "express"
+import RestaurantsCtrl from "./restaurants.controller.js"
+import ReviewsCtrl from "./reviews.controller.js"
 
-
-const router = express.Router();
+const router = express.Router()
 
 router.route("/").get(RestaurantsCtrl.apiGetRestaurants)
+router.route("/id/:id").get(RestaurantsCtrl.apiGetRestaurantById)
+router.route("/cuisines").get(RestaurantsCtrl.apiGetRestaurantCuisines)
 
 router
-    .route("/review")
-    .post(ReviewsCtrl.apiPostReview)
-    .put(ReviewsCtrl.apiUpdateReview)
-    .delete(ReviewsCtrl.apiDeleteReview)
+  .route("/review")
+  .post(ReviewsCtrl.apiPostReview)
+  .put(ReviewsCtrl.apiUpdateReview)
+  .delete(ReviewsCtrl.apiDeleteReview)
 
-export default router;
+export default router
--- backend/dao/restaurantsDAO.js
+++ backend/dao/restaurantsDAO.js
@@ -1,5 +1,5 @@
 import mongodb from "mongodb"
-const ObjectId = mongodb.ObjectID
+const ObjectId = mongodb.ObjectId
 let restaurants
 
 export default class RestaurantsDAO {
@@ -55,5 +55,59 @@ export default class RestaurantsDAO {
       )
       return { restaurantsList: [], totalNumRestaurants: 0 }
     }
-}
+  }
+  static async getRestaurantByID(id) {
+    try {
+      const pipeline = [
+        {
+            $match: {
+                _id: new ObjectId(id),
+            },
+        },
+              {
+                  $lookup: {
+                      from: "reviews",
+                      let: {
+                          id: "$_id",
+                      },
+                      pipeline: [
+                          {
+                              $match: {
+                                  $expr: {
+                                      $eq: ["$restaurant_id", "$$id"],
+                                  },
+                              },
+                          },
+                          {
+                              $sort: {
+                                  date: -1,
+                              },
+                          },
+                      ],
+                      as: "reviews",
+                  },
+              },
+              {
+                  $addFields: {
+                      reviews: "$reviews",
+                  },
+              },
+          ]
+      return await restaurants.aggregate(pipeline).next()
+    } catch (e) {
+      console.error(`Something went wrong in getRestaurantByID: ${e}`)
+      throw e
+    }
+  }
+
+  static async getCuisines() {
+    let cuisines = []
+    try {
+      cuisines = await restaurants.distinct("cuisine")
+      return cuisines
+    } catch (e) {
+      console.error(`Unable to get cuisines, ${e}`)
+      return cuisines
+    }
+  }
 }
