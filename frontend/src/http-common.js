dda3302ff474d03ac7c40d3eb05773cfffca5868
gary <gary@ligginsdevelopment.com>
2021-10-19 15:47:07 -0700

    changed httpcommon to heroku

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:5000/api/v1/restaurants',
+    baseURL: "https://mern-restaurants-app.herokuapp.com/",
     headers: {
         'Content-type': 'application/json'
     }
 });
+
+
+
+// 'http://localhost:5000/api/v1/restaurants'
